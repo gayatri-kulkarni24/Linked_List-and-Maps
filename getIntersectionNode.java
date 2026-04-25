354adf5ceda48d5950017ef812ea3b1f1aebab4b
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-04-25 19:32:01 +0530

    Create getIntersectionNode.java

---
+++ getIntersectionNode.java
@@ -0,0 +1,14 @@
+    public Node getIntersectionNode(Node head1,Node head2){
+        
+        while(head2!=null){
+            Node temp=head1;
+            while(temp!=null){
+                if(temp==head2){
+                return head2;
+            }
+            }
+            
+            head2=head2.next;
+        }
+        return null;
+    }
